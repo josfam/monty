eed14fe6a2998bc38b7a220d732f1d9c24884c0b
josfam <josephamukun@tutanota.com>
2024-01-17 18:18:17 +0300

    Add funcs that mutate to a separate file

---
+++ _mutating.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * swap - Swaps the top two elements of the stack
+ * @tail: The tail of the stack where swapping will happen
+ * @line_number: The line number being processed in the monty bytecode file
+ * Description: Swaps the top two elements of the stack
+ * Return: Nothing
+ */
+void swap(stack_t **tail, unsigned int line_number)
+{
+	/* TODO */
+}
