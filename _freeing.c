92abea9e3121344e67cb49364ad5c2604d00eea7
josfam <josephamukun@tutanota.com>
2024-01-17 17:09:27 +0300

    Refactor: Move freeing code to its own file

---
+++ _freeing.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * free_stack - Frees the stack memory
+ * @tail: The tail of the stack to free
+ * Description: Frees the stack memory
+ * Return: Nothing
+ */
+void free_stack(stack_t *tail)
+{
+	/* TODO */
+}
--- _stack.c
+++ _stack.c
@@ -36,14 +36,3 @@ void swap(stack_t **tail, unsigned int line_number)
 {
 	/* TODO */
 }
-
-/**
- * free_stack - Frees the stack memory
- * @tail: The tail of the stack to free
- * Description: Frees the stack memory
- * Return: Nothing
- */
-void free_stack(stack_t *tail)
-{
-	/* TODO */
-}
