a2a762f0b9502b315f89070f1775e5b99ff9570a
josfam <josephamukun@tutanota.com>
2024-01-18 11:53:51 +0300

    Move inspection functions to their own file

---
+++ _inspecting.c
@@ -0,0 +1,15 @@
+#include <stddef.h>
+#include "monty.h"
+
+/**
+ * is_empty - Checks if the stack is empty
+ * @tail: The tail of the stack
+ * Description: Checks if the stack is empty
+ * Return: 1 if the stack is empty, 0 otherwise
+ */
+int is_empty(stack_t **tail)
+{
+	if (tail == NULL)
+		return (1);
+	return (0);
+}
