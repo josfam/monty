372862e664cad5fdbf2a22bac18ec6702aacb483
josfam <josephamukun@tutanota.com>
2024-01-18 20:18:08 +0300

    Add basic instruction checks in main

--- monty.c
+++ monty.c
@@ -12,8 +12,44 @@
 int main(int argc, char *argv[])
 {
 	char **instruction;
+	char *line, *opcode, *arg;
+	char buffer[MAX_LINE_LEN];
+	int line_num;
 	FILE *fp;
 	stack_t *tail;
+
+	check_args(argc);
+	fp = get_file(argv);
+
+	tail = NULL;
+
+	/* read lines from the monty byte file */
+	while (1)
+	{
+		line = fgets(buffer, sizeof(buffer), fp);
+		if (!line)
+			break;
+
+		line_num++;
+		instruction = extract_instruction(line);
+		opcode = instruction[0];
+		arg = instruction[1];
+
+		if (!opcode) /* there was no instruction */
+			continue;
+		else if (!(is_legal(opcode)))
+		{
+			fprintf(stderr, "L%d: unknown instruction %s\n", line_num, opcode);
+			exit(EXIT_FAILURE);
+		}
+		else if (is_lone(opcode))
+		{
+			/* TODO */
+		}
+		else
+		{
+			/* TODO */
+		}
 	}
 
 	fclose(fp);
---
+++ tests/bytecode_files/invalid.m
@@ -0,0 +1,10 @@
+             push 0         
+push 1
+		break
+whisk 8
+push 1
+push 2
+pour          3
+fry            
+
+
