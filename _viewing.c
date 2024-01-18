43d172f8237b3230cb79585e2043fd0335db1184
josfam <josephamukun@tutanota.com>
2024-01-18 12:09:55 +0300

    Remove inspection code from viewing file

--- _viewing.c
+++ _viewing.c
@@ -29,15 +29,3 @@ void pall(stack_t **tail)
 	}
 }
 
-/**
- * is_empty - Checks if the stack is empty
- * @tail: The tail of the stack
- * Description: Checks if the stack is empty
- * Return: 1 if the stack is empty, 0 otherwise
- */
-int is_empty(stack_t **tail)
-{
-	if (tail == NULL)
-		return (1);
-	return (0);
-}
